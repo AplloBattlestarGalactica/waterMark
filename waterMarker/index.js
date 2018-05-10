9b3ddcb96bb31257110afd4138c1ea98b18a098b
josexiaohang <jose.xiaohang@gmail.com>
2018-05-10 15:03:08 +0800

    bugfix

--- waterMarker/index.js
+++ waterMarker/index.js
@@ -52,11 +52,11 @@ class WaterMarker {
         let config = { attributes: true, childList: true, characterData: true };
 
         canvasObserver.observe(document.querySelector('#divContainer').parentNode, { childList: true });
-        let canvasObserverChild = new MutationObserver(mo=>{
+        let canvasObserverChild = new MutationObserver(mo => {
             this.drawCanvas()
         })
-        let configChild = {attributes: true, childList: true, characterData: true };
-        canvasObserverChild.observe(document.querySelector('#divContainer',configChild))
+        let configChild = { attributes: true, childList: true, characterData: true };
+        canvasObserverChild.observe(document.querySelector('#divContainer'), configChild)
     }
 }
 export default WaterMarker;
