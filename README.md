9d52afc5e5be8379c718b9eb82ff14a610cb38d3
josexiaohang <jose.xiaohang@gmail.com>
2018-05-10 14:59:19 +0800

    watermarker cant delete

--- README.md
+++ README.md
@@ -5,20 +5,22 @@
 
 * npm install watermarker --save
 * import WaterMarker from 'screenwatermark'
-* const WaterMarker = new WaterMarker()
-* WaterMarker.drawCanvas({
-            font: '10px Microsoft YaHei',//字体 canvas font
-            textAlign: 'center', //字体位置 canvas textAlign
-            textStyle: '#9e9e9e4f', //字体颜色 canvas textcolor
-            text: 'this is water marker test', //内容 canvas textContent
-            degree: 20, //旋转角度 canvas rotate
-            canvasWidth: 400, //canvas宽 canvasWidth 
-            canvasHeight: 400, //canvas高 canvasHeight
-            })
+* const WaterMarker = new WaterMarker({  
+* font: '18px Microsoft YaHei',//字体 canvas font
+* textAlign: 'center', //字体位置 canvas textAlign
+* textStyle: '#9e9e9e8a', //字体颜色 canvas textcolor
+* text: 'this is water marker test', //内容 canvas textContent
+* degree: 20, //旋转角度 canvas rotate
+* canvasWidth: 400, //canvas宽 canvasWidth 
+* canvasHeight: 400, //canvas高 canvasHeight})
+* WaterMarker.canvasObserver({
+ })
 
 ## function describe功能描述
 * 屏幕水印插件，支持字体，字号，内容位置，内容颜色，canvas角度，并且通过canvas宽高来定义水印离散程度。WaterMarker Tool support canvasFont,canvasFontTextAlign,canvasTextStyle,canvasDegree and you can change canvasWidth/height to dispersed your watermatker.
-## coming soon 水印不可删除，隐藏式水印（waterMarker can't be deleted and concealment watermarker）
+
+* 水印不可删除，watermarker can't be deleted
+## coming soon ，隐藏式水印（ concealment watermarker）
 ## how about a Star https://github.com/AplloBattlestarGalactica/waterMark  
 ## tell me your issue
 ## etc..
--- waterMarker/index.js
+++ waterMarker/index.js
@@ -1,44 +1,62 @@
 import './index.css';
 class WaterMarker {
-    constructor() {
-        this.option = {
-            font: '10px Microsoft YaHei',
-            textAlign: 'center',
-            textStyle: '#9e9e9e4f',
-            text: 'this is water marker test',
-            degree: 20,
-            canvasWidth: 400,
-            canvasHeight: 400,
-        }
+    constructor(opt, isOberserve) {
+        this.option = opt;
+        this.isOberserve = isOberserve;
         this.drawCanvas = this.drawCanvas.bind(this);
-        this.divContainer = document.body.appendChild(document.createElement('div'));
-        this.waterMarkercanvas = document.createElement('canvas');
-        this.context = this.waterMarkercanvas.getContext('2d');
+        this.canvasObserver = this.canvasObserver.bind(this);
+        this.canvasState = false;
 
-        this.divContainer.appendChild(this.waterMarkercanvas);
-        this.divContainer.id = 'divContainer'
-        this.divContainer.style.height = window.innerHeight+'px';
-        this.divContainer.style.width = window.innerWidth+'px';
-        this.backgroundUrl = null;
     }
 
-    drawCanvas(opt) {
+    drawCanvas() {
 
-        this.waterMarkercanvas.width = opt.canvasWidth || this.option.width;
-        this.waterMarkercanvas.height = opt.canvasHeight || this.option.height;
+        let divContainer = document.body.appendChild(document.createElement('div'));
+        let waterMarkercanvas = document.createElement('canvas');
+        let context = waterMarkercanvas.getContext('2d');
+
+        divContainer.appendChild(waterMarkercanvas);
+        divContainer.id = 'divContainer'
+        let backgroundUrl = null;
+
+        divContainer.style.height = window.innerHeight + 'px';
+        divContainer.style.width = window.innerWidth + 'px';
+
+
+        waterMarkercanvas.width = this.option.canvasWidth;
+        waterMarkercanvas.height = this.option.canvasHeight;
 
 
 
-        this.context.font = opt.font || this.option.font;
-        this.context.textAlign = opt.textAlign || this.option.textAlign;
-        this.context.fillStyle = opt.textStyle || this.option.textStyle;
-        this.context.translate(this.waterMarkercanvas.width / 2, this.waterMarkercanvas.height / 2);
-        this.context.rotate(opt.degree * Math.PI / 180);
-        this.context.fillText(opt.text || this.opt.text, 0, 0);
+        context.font = this.option.font;
+        context.textAlign = this.option.textAlign;
+        context.fillStyle = this.option.textStyle;
+        context.translate(waterMarkercanvas.width / 2, waterMarkercanvas.height / 2);
+        context.rotate(this.option.degree * Math.PI / 180);
+        context.fillText(this.option.text, 0, 0);
 
-        this.backgroundUrl = this.waterMarkercanvas.toDataURL('image/png');
-        this.divContainer.style.backgroundImage = `url(${this.backgroundUrl})`;
+        backgroundUrl = waterMarkercanvas.toDataURL('image/png');
+        divContainer.style.backgroundImage = `url(${backgroundUrl})`;
+        this.canvasState = true;
 
+
+    }
+
+    canvasObserver() {
+        this.drawCanvas();
+        let canvasObserver = new MutationObserver((mo) => {
+            if (mo[0].removedNodes.id = 'divContainer' && mo[0].addedNodes.length == 0 && mo[0].previousSibling != null) {
+                this.drawCanvas()
+            }
+        });
+        let config = { attributes: true, childList: true, characterData: true };
+
+        canvasObserver.observe(document.querySelector('#divContainer').parentNode, { childList: true });
+        let canvasObserverChild = new MutationObserver(mo=>{
+            this.drawCanvas()
+        })
+        let configChild = {attributes: true, childList: true, characterData: true };
+        canvasObserverChild.observe(document.querySelector('#divContainer',configChild))
     }
 }
 export default WaterMarker;
